705bf75cd224bd1dcdf3d0f9e7bddc78faf63474
seonghoYi <sh.Lee4781@gmail.com>
2022-01-14 21:16:38 +0900

    i2cBegin 동작주파수 인수 추가

--- src/common/hw/include/i2c.h
+++ src/common/hw/include/i2c.h
@@ -16,7 +16,7 @@ extern "C" {
 
 
 bool i2cInit();
-bool i2cBegin(uint8_t ch);
+bool i2cBegin(uint8_t ch, uint32_t freq_khz);
 uint32_t i2cWrite(uint8_t ch, uint16_t dev_addr, uint8_t *p_data, uint16_t size);
 uint32_t i2cRead(uint8_t ch, uint16_t dev_addr, uint8_t *p_data, uint16_t size);
 uint32_t i2cMemWrite(uint8_t ch, uint16_t dev_addr, uint16_t mem_addr, uint16_t mem_addr_size, uint8_t *p_data, uint16_t size);
