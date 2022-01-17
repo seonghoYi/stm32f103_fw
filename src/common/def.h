be16829f219a6641eef771593c67f5c3ac483fbf
seonghoYi <sh.Lee4781@gmail.com>
2022-01-17 23:40:34 +0900

    ili9481 추가

--- src/common/def.h
+++ src/common/def.h
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 
 
+
+
 #define _DEF_I2C1		0
 #define _DEF_I2C2		1
 
@@ -16,6 +18,12 @@
 #define _DEF_LCD2		1
 
 
+
+#define _DEF_OUTPUT	0
+#define _DEF_INPUT	1
+
+#define _DEF_LOW		0
+#define _DEF_HIGH		1
 
 
 #endif /* SRC_COMMON_DEF_H_ */
--- src/hw/hw_def.h
+++ src/hw/hw_def.h
@@ -15,8 +15,14 @@
 #define			 HW_SSD1306_HEIGHT			64
 
 
+#define _USE_HW_ILI9481
+#define			 HW_ILI9481_WIDTH				480
+#define 		 HW_ILI9481_HEIGHT			320
+#define			 HW_ILI9481_GPIO_MAX_CH	13
+
+
 #define _USE_HW_LCD
-#define 		 HW_LCD_CH_MAX					1
+#define 		 HW_LCD_CH_MAX					2
 
 
 #endif /* SRC_HW_HW_DEF_H_ */
